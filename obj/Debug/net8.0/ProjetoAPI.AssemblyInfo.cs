19be298a008a6aed5c628ede5bdc580a7d743a87
Fernanda costa <fernandinhacosta.97@hotmail.com>
2024-04-29 22:57:47 -0300

    subindo ultima versao pra testes

--- obj/Debug/net8.0/ProjetoAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjetoAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c0f1b07b7066e6eeb6604d892c81a3c90dd23cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51aeef90f4fa0bb423d4e447d1a01af200cdec58")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
